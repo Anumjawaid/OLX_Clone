8e0120c00bf90bd841ef67add71d7bd1b5ecb4d5
Anumjawaid <anumjawaid99@gmail.com>
2020-11-14 13:25:30 +0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './App.css';
-import  Header from './components/header'
+import  {Header,Homeimg,Contgrey,Cont} from './components/header'
+import Home from './components/home'
 
 function App() {
   return (
     <div className="App">
    <Header/>
+   <br></br>
+   <br></br>
+   <br></br>
+<Home />
+<br></br>
+<Homeimg />
+<Contgrey />
+<Cont />
+
+
     </div>
   );
 }
