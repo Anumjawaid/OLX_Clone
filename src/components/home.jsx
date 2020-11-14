db02b49b1a537cdb07dcffb9b639885d1f53c4be
Anumjawaid <anumjawaid99@gmail.com>
2020-11-14 13:26:53 +0500

    only bootstrap

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,28 +9,36 @@ constructor(){
 render(){
     return(
         <div>
-            
-<div>
-<nav className="navbar navbar-light bg-light">
-  <a className="navbar-brand" href="#">
-    <img src={olx} width="70" height="30" alt="" loading="lazy"/>
-  </a>
-  <ul className="navbar-nav mr-auto"> 
-      <li className="nav-item">
-      <select id="inputState" className="form-control s1">
-        <option selected><i className="fas fa-search"></i>Choose...</option>
-        <option>...</option>
-      </select>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li className="nav-item">
-      <select id="inputState" className="form-control s2">
-        <option selected>Find Cars,Mobile Phones and more...</option>
-        <option>...</option>
-      </select>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
       </li>
-      </ul>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
 </nav>
-</div>
+
 
         </div>
     )
